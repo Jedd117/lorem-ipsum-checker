2fd7cee3a73b08384630782e8569e0e678c950e4
Jeff Baxter <jeffbaxter@live.com>
2021-05-10 17:41:26 -0400

    Update lorem-checker.py

--- lorem-checker.py
+++ lorem-checker.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
+links = []
+
 #opens urls
 f = open('urls.txt', 'r')
 
@@ -10,8 +12,6 @@ for line in f:
     # Removing new line from lines
     line = line.rstrip('\n')
 
-    print("\nChecking " + line)
-
     # Go to URL
     req = requests.get(line)
     soup = BeautifulSoup(req.content, 'html.parser')
@@ -19,9 +19,9 @@ for line in f:
 
     # If text has has Lorem Ipsum, 
     if soup.find(string=re.compile("lorem",re.IGNORECASE)):
-        print('\nThese links have lorem ipsum: ')
-        print(line)
-    else:
-        print("\nNo lorem ipsum detected...")
+        links.append(line)
+    
+print("These links have Lorem Ipsum:")
+print('\n'.join(map(str,links)))
 
 f.close()
